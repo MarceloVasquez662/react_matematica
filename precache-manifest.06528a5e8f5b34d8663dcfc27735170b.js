c0baca16133b8c77c571b1d045f0aa3091dc6927
MarceloVasquez662 <marcelovasquez662@gmail.com>
2020-06-21 14:34:35 -0400

    Updates

---
+++ precache-manifest.06528a5e8f5b34d8663dcfc27735170b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181638c1249a583ba88bfe9621527ffc",
+    "url": "/MarceloVasquez662/react_matematica.git/index.html"
+  },
+  {
+    "revision": "e9bffa25d3be08e80b47",
+    "url": "/MarceloVasquez662/react_matematica.git/static/css/main.4a92dfd0.chunk.css"
+  },
+  {
+    "revision": "0f48b6a728852e96db6b",
+    "url": "/MarceloVasquez662/react_matematica.git/static/js/2.c08d1737.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/MarceloVasquez662/react_matematica.git/static/js/2.c08d1737.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9bffa25d3be08e80b47",
+    "url": "/MarceloVasquez662/react_matematica.git/static/js/main.c64799b7.chunk.js"
+  },
+  {
+    "revision": "b22be03f8fd54258c9a7",
+    "url": "/MarceloVasquez662/react_matematica.git/static/js/runtime-main.20dcc451.js"
+  },
+  {
+    "revision": "203aa84f70a1b725557ef3f5d3e3dc29",
+    "url": "/MarceloVasquez662/react_matematica.git/static/media/fondo.203aa84f.png"
+  }
+]);
